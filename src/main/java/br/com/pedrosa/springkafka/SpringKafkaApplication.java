fc08cd7431f27cf6a27fe1070a2a5e0cd5eca4f1
Fabio Henrique Sampaio Pedrosa <bito.pedrosa@gmail.com>
2020-04-01 17:45:36 -0300

    first version with kafka

--- src/main/java/br/com/pedrosa/springkafka/SpringKafkaApplication.java
+++ src/main/java/br/com/pedrosa/springkafka/SpringKafkaApplication.java
@@ -1,21 +1,7 @@
 package br.com.pedrosa.springkafka;
 
-import br.com.pedrosa.springkafka.dto.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.support.converter.DefaultJackson2JavaTypeMapper;
-import org.springframework.kafka.support.converter.Jackson2JavaTypeMapper;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
